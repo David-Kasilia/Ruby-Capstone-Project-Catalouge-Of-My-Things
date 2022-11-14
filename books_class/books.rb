e52b0435584ecfd2fb8c8f15e03b760451b133b7
David-Kasilia <davidkasilia001@gmail.com>
2022-11-14 20:21:55 +0300

    fix rubocop linters

--- books_class/books.rb
+++ books_class/books.rb
@@ -1,18 +1,17 @@
 require_relative '../item'
 
 class Book < Item
+  attr_accesor :name, :publish_date, :publisher, :cover_state
 
-    attr_accesor :name, :publish_date, :publisher, :cover_state
+  def initialize(name, publish_date, publisher, cover_state, id = Random.rand(1..1000))
+    super(id, name, publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
 
-    def initialize(id = Random.rand(1..1000), name, publish_date, publisher, cover_state)
-        super(id, name, publish_date)
-        @publisher = publisher
-        @cover_state = cover_state
-    end
+  private
 
-    private
-    def can_be_archived?
-        super || @cover_state == 'bad'
-    end
-
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
 end
---
+++ books_class/label.rb
@@ -0,0 +1,16 @@
+class Label
+  attr_accessor :title, :color, :items
+  attr_reader :id
+
+  def initialize(_id, title, color)
+    @id = Random.rand(1..1000)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    items.add_label(self)
+  end
+end
