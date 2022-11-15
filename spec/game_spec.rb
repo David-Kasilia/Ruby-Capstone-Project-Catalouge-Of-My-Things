121c42948d407c01ac162af85739afc6191fbec3
Alusp <alusinejalloh800@gmail.com>
2022-11-15 15:47:40 +0200

    Write test for game

---
+++ spec/game_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../game'
+require 'date'
+
+describe Game do
+  context 'when the game is not archived' do
+    archived = true
+    multiplayer = true
+    last_played_at = 2018
+    name = 'Super Mario'
+    publish_date = Date.parse('2018-01-01')
+    game = Game.new( name, multiplayer, last_played_at, publish_date, archived )
+    it 'should not be archived' do
+      expect(game.archived).to eq(true)
+    end
+    it 'should create a new game' do
+      expect(game).to be_an_instance_of(Game)
+    end
+  end
+  end
